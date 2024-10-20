b2e5b147f6bc98b0fc02e50854bc7b2011003cd7
Farhan0511 <programingsukses.com>
2024-10-20 22:44:49 +0700

    add service views

---
+++ src/views/Service.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <navbar/>
+  </div>
+
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+    name : 'Service',
+    components: {
+        Navbar
+    }
+}
+</script>
+
+<style>
+
+</style>
